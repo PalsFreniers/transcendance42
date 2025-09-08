8ae62aade6e5c1b81616f4185a6e7534419781e4
maxouvra <maxenceouvrarddu16@outlook.fr>
2025-09-08 15:44:26 +0200

    music

--- frontend/src/client/navClient.ts
+++ frontend/src/client/navClient.ts
@@ -256,6 +256,11 @@ export function handleRoute() {
 				<div id="barres"></div>
 				<div id="border"></div>
 				<div id="profils">
+					<div id="music-player">
+  						<audio id="music" preload="auto"></audio>
+  						<svg id="btn-music" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#e323be"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="" stroke-width="0.4800000000000001"></g><g id="SVGRepo_iconCarrier"><path d="M14.3187 2.50498C13.0514 2.35716 11.8489 3.10033 11.4144 4.29989C11.3165 4.57023 11.2821 4.86251 11.266 5.16888C11.2539 5.40001 11.2509 5.67552 11.2503 6L11.25 6.45499C11.25 6.4598 11.25 6.4646 11.25 6.46938V14.5359C10.4003 13.7384 9.25721 13.25 8 13.25C5.37665 13.25 3.25 15.3766 3.25 18C3.25 20.6234 5.37665 22.75 8 22.75C10.6234 22.75 12.75 20.6234 12.75 18V9.21059C12.8548 9.26646 12.9683 9.32316 13.0927 9.38527L15.8002 10.739C16.2185 10.9481 16.5589 11.1183 16.8378 11.2399C17.119 11.3625 17.3958 11.4625 17.6814 11.4958C18.9486 11.6436 20.1511 10.9004 20.5856 9.70089C20.6836 9.43055 20.7179 9.13826 20.7341 8.83189C20.75 8.52806 20.75 8.14752 20.75 7.67988L20.7501 7.59705C20.7502 7.2493 20.7503 6.97726 20.701 6.71946C20.574 6.05585 20.2071 5.46223 19.6704 5.05185C19.4618 4.89242 19.2185 4.77088 18.9074 4.6155L16.1999 3.26179C15.7816 3.05264 15.4412 2.88244 15.1623 2.76086C14.8811 2.63826 14.6043 2.53829 14.3187 2.50498Z" fill="#000"></path> </g></svg>
+  						<input type="range" id="volume-music" min="0" max="1" step="0.01" value="0.5"/>
+					</div>
 					<div id="parametre">
 						<svg fill="#000000" viewBox="-2.4 -2.4 28.80 28.80" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00024000000000000003" transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#e323be" stroke-width="0.9600000000000002"> <g data-name="Layer 2"> <g data-name="options-2"> <rect width="24" height="24" transform="rotate(90 12 12)" opacity="0"></rect> <path d="M19 9a3 3 0 0 0-2.82 2H3a1 1 0 0 0 0 2h13.18A3 3 0 1 0 19 9zm0 4a1 1 0 1 1 1-1 1 1 0 0 1-1 1z"></path> <path d="M3 7h1.18a3 3 0 0 0 5.64 0H21a1 1 0 0 0 0-2H9.82a3 3 0 0 0-5.64 0H3a1 1 0 0 0 0 2zm4-2a1 1 0 1 1-1 1 1 1 0 0 1 1-1z"></path> <path d="M21 17h-7.18a3 3 0 0 0-5.64 0H3a1 1 0 0 0 0 2h5.18a3 3 0 0 0 5.64 0H21a1 1 0 0 0 0-2zm-10 2a1 1 0 1 1 1-1 1 1 0 0 1-1 1z"></path> </g> </g> </g><g id="SVGRepo_iconCarrier"> <g data-name="Layer 2"> <g data-name="options-2"> <rect width="24" height="24" transform="rotate(90 12 12)" opacity="0"></rect> <path d="M19 9a3 3 0 0 0-2.82 2H3a1 1 0 0 0 0 2h13.18A3 3 0 1 0 19 9zm0 4a1 1 0 1 1 1-1 1 1 0 0 1-1 1z"></path> <path d="M3 7h1.18a3 3 0 0 0 5.64 0H21a1 1 0 0 0 0-2H9.82a3 3 0 0 0-5.64 0H3a1 1 0 0 0 0 2zm4-2a1 1 0 1 1-1 1 1 1 0 0 1 1-1z"></path> <path d="M21 17h-7.18a3 3 0 0 0-5.64 0H3a1 1 0 0 0 0 2h5.18a3 3 0 0 0 5.64 0H21a1 1 0 0 0 0-2zm-10 2a1 1 0 1 1 1-1 1 1 0 0 1-1 1z"></path> </g> </g> </g></svg>
 					</div>
@@ -431,5 +436,40 @@ export function handleRoute() {
 			alert("Logout failed: " + (data.error || "Unknown error"));
 		}
 	});
+
+	const music = document.getElementById("music") as HTMLAudioElement;
+	const musicButton = document.getElementById("btn-music") as HTMLButtonElement;
+	const musicVolume = document.getElementById("volume-music") as HTMLInputElement;
+
+	let currentTrack = 0;
+	let isPlaying = false;
+
+	const playlist = 
+	[
+		"/asset/Nightcore - Love Is Confusing (Lyrics).mp3"
+	]
+
+	music!.src = playlist[currentTrack];
+	music.addEventListener("ended", () => {
+		currentTrack = (currentTrack + 1) % playlist.length;
+		music.src = playlist[currentTrack];
+		music.play();
+	});
+
+	musicButton.addEventListener("click", () => {
+		if (isPlaying){
+			music.pause();
+			musicButton.classList.add("paused");
+		}
+		else{
+			music.play();
+			musicButton.classList.remove("paused");
+		}
+		isPlaying = !isPlaying;
+	});
+
+	musicVolume?.addEventListener("input", () => {
+		music.volume = Number(musicVolume.value);
+	})
 }
 
